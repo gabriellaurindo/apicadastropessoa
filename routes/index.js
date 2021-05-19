5ae493df310f8c84317bb5a92a936d352b743804
Gabriel Laurindo <gabrielvitorlaurindo@gmail.com>
2021-05-19 19:25:51 -0300

    Rota inicial criada

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 //Importando Módulos
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt')
-const User = require("../api/models/User")
 
 //Rota Raiz
 router.get('/', function (req, res,) {
-   res.send({Descrição:'API REST Privada!', Version: '1.0.0'})
+   res.send({Descrição:'API REST Publica!, Não salve informações confidenciais!!', Rotas:'/cadastro, /consultar' , Version: '1.0.0'})
 })
 
 
-   
 //Exportando Rota Raiz
 module.exports = router
--- src/app.js
+++ src/app.js
@@ -11,22 +11,11 @@ app.use(express.json());
 //Config cors
 app.use(cors());
 
-
 //Importar Rotas
 const index =   require('../routes/index')
-const login =   require('../routes/login')
-const cadastro_os = require('../routes/cadastro-os')
-const pesquisa_os = require('../routes/pesquisa-os')
-const cadastro_cliente = require('../routes/cadastro-client')
-const pesquisa_cliente = require('../routes/pesquisa-cliente')
 
 //Usar Rotas
 app.use('/',index)
-app.use('/',login)
-app.use('/',cadastro_os)
-app.use('/',pesquisa_os)
-app.use('/',cadastro_cliente)
-app.use('/',pesquisa_cliente)
 
 //Importando Banco
 require("../config/database")
